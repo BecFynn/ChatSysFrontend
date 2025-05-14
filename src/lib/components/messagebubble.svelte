ddbafcfbecabc62eca3ebe61bc39d798bc746d3d
EYF1FE <EYF1FE@FEBO-C-00156>
2025-05-14 15:16:02 +0200

    frontend update

---
+++ src/lib/components/messagebubble.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import type {MessageDTO} from "$lib/api/Api";
+    export let message
+    //falls benutzer id eigene benutzerID 
+    let left=false
+</script>
+<div class="flex"  class:justify-end={left}>
+<div class="bg-lightgray bg-gray-300 w-auto p-2 m-2 rounded-lg ">
+    {message}
+</div>
+</div>
