c074ba0476f8549e4c02956616fb0bdd30fd8193
EYF1FE <EYF1FE@FEBO-C-00156>
2025-05-21 15:19:36 +0200

    adding fixed

--- src/lib/components/AddToGroup.svelte
+++ src/lib/components/AddToGroup.svelte
@@ -41,11 +41,15 @@
 		//console.log(adding);
     }
 	
-
     let users: UserDTO[] = [];
+	let usersInGroup: GroupchatDTO;
     onMount(async () => {
 		const userList = await api.userList().then(r => r.data)
 		users = userList
+		
+		const group = await api.id.groupDetail(String(target?.id)).then(r => r.data)
+		usersInGroup =group
+		console.log(group)
 	});
 </script>
 
@@ -55,6 +59,19 @@
 	<div>Manage User</div>
 	<!--hidden-->
 	<div class=" hidden absolute bg-gray-500 w-90 overflow-x-hidden no-scrollbar overflow-y-auto h-55  group-hover:block top-full left-0 z-10 rounded-b-md">
+		{#if usersInGroup?.users}
+		{#each usersInGroup?.users as person}
+			<div class="flex flex-row justify-between gap-2 p-2 font-normal text-sm text-white">
+				<div class="flex flex-row ">
+					<img src={person.avatar} alt={person?.id} class="h-8 w-8 rounded-full" />
+					{person.displayName}
+				</div>
+				<div>
+					<a class="p-3 rounded-lg hover:bg-red-300" href="#" on:click|preventDefault={() => removeUser(person)}>-</a>
+				</div>
+			</div>
+		{/each}
+		{/if}
 		{#each users as person}
 			<div class="flex flex-row justify-between gap-2 p-2 font-normal text-sm text-white">
 				<div class="flex flex-row ">
@@ -62,8 +79,7 @@
 					{person.displayName}
 				</div>
 				<div>
-					<a class="p-3 hover:bg-gray-300" href="#" on:click|preventDefault={() => addUser(person)}>+</a>
-					<a class="p-3 hover:bg-gray-300" href="#" on:click|preventDefault={() => removeUser(person)}>-</a>
+					<a class="p-3 rounded-lg hover:bg-green-300" href="#" on:click|preventDefault={() => addUser(person)}>+</a>
 				</div>
 			</div>
 		{/each}
